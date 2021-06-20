9008cf3dfa623cb928bd20db94fb61c5d6fd98d7
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2021-06-20 11:37:05 +0200

    Successfully Made the API Call.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function onSearchSubmit() {
     var name = document.getElementById("movieName").value;
     var modifiedName = name.split(' ').join('_');
+    makeAPICall(modifiedName).then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.log(error.message);
+    })
 }
 
 async function makeAPICall(movieName) {
-    const response = await fetch(`http://www.omdbapi.com/?t=${movieName}`);
+    const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=6ea51b5&t=${movieName}`);
     const data = await response.json();
     return data;
 }
