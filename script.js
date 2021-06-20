6cde08e9ea0985995a5e1ac2ecbe685c81ee9d9a
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2021-06-20 11:28:53 +0200

    Added Method for the API Call.

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function onSearchSubmit() {
     var name = document.getElementById("movieName").value;
     var modifiedName = name.split(' ').join('_');
 }
+
+async function makeAPICall(movieName) {
+    const response = await fetch(`http://www.omdbapi.com/?t=${movieName}`);
+    const data = await response.json();
+    return data;
+}
